64277793c81ea2de826b764d35ecbb9cd66b72af
Christopher Grady <grittygrady@gmail.com>
2021-05-11 12:41:59 -0700

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className='Header border1'>
+      <h1>HEROINfo</h1>
+    </header>
+  );
+};
+
+export default Header;
