38912db935d5fc30407ee6638466d6f69dd01477
Christopher Grady <grittygrady@gmail.com>
2021-05-14 09:33:30 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './App.css';
-import config from './config';
 import Header from './components/Header';
 import SearchForm from './components/SearchForm';
 import HeroCard from './components/HeroCard';
