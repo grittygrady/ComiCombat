12ccb08c03163ea175c8faf74532b0bd87f957c1
Christopher Grady <grittygrady@gmail.com>
2021-05-14 10:09:46 -0700

    Commi

--- src/components/HeroCard.js
+++ src/components/HeroCard.js
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-import './HeroCard.css';
-import STORE from '../STORE';
-
-const HeroCard = () => {
-  return (
-    <>
-      <h1>HERO CARD</h1>
-      {STORE.results.map((hero) => (
-        <div className='herocard' key={hero.id}>
-          <div className='herocard__title'>
-            <h2>{hero.name}</h2>
-            <p>Real name: {hero.biography['full-name']}</p>
-            <p>Birth: {hero.biography['place-of-birth']}</p>
-            <p>First Appearance: {hero.biography['first-appearance']}</p>
-            <p>Group Affiliation: {hero.connections['group-affiliation']}</p>
-            <div className='powerstats'>
-              <h4>Powerstats: </h4>
-              <ul>
-                <li>COMBAT: {hero.powerstats.combat}</li>
-                <li>STRENGTH: {hero.powerstats.strength}</li>
-                <li>POWER: {hero.powerstats.power}</li>
-                <li>INTELLIGENCE: {hero.powerstats.intelligence}</li>
-                <li>SPEED: {hero.powerstats.speed}</li>
-                <li>DURABILITY: {hero.powerstats.durability}</li>
-              </ul>
-            </div>
-          </div>
-          <div className='herocard__img'>
-            <img src={hero.image.url} alt={hero.name} />
-          </div>
-        </div>
-      ))}
-    </>
-=======
 import { Card, Icon, Image } from 'semantic-ui-react';
 import './HeroCard.css';
 const HeroCard = (props) => {
@@ -76,7 +41,6 @@ const HeroCard = (props) => {
         </ul>
       </Card.Content>
     </Card>
->>>>>>> parent of 04cb3f7 (Styling Hero Icons)
   );
 };
 
