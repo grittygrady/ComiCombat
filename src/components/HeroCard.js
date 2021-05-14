81239eefb76e8c28515495a00a4a19da68d94beb
Christopher Grady <grittygrady@gmail.com>
2021-05-14 09:57:21 -0700

    Commi

--- src/components/HeroCard.js
+++ src/components/HeroCard.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import './HeroCard.css';
 import STORE from '../STORE';
 
@@ -31,6 +32,51 @@ const HeroCard = () => {
         </div>
       ))}
     </>
+=======
+import { Card, Icon, Image } from 'semantic-ui-react';
+import './HeroCard.css';
+const HeroCard = (props) => {
+  const {
+    name,
+    id,
+    realName,
+    birthplace,
+    firstAppearance,
+    groupAffiliation,
+    combatStat,
+    strengthStat,
+    powerStat,
+    intelligenceStat,
+    speedStat,
+    durabilityStat,
+    imageUrl,
+  } = props;
+  return (
+    <Card key={id}>
+      <Image src={imageUrl} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{name}</Card.Header>
+        <Card.Meta>Real Name: {realName}</Card.Meta>
+        <Card.Meta>Birth: {birthplace}</Card.Meta>
+        <Card.Description className='card__description'>
+          <strong>First Appearance:</strong> {firstAppearance}
+          <br />
+          <strong>Group Affiliation:</strong> {groupAffiliation}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content>
+        <h4>POWER STATS</h4>
+        <ul className='powerstatlist'>
+          <li>COMBAT: {combatStat}</li>
+          <li>STRENGTH: {strengthStat}</li>
+          <li>POWER: {powerStat}</li>
+          <li>INTELLIGENCE: {intelligenceStat}</li>
+          <li>SPEED: {speedStat}</li>
+          <li>DURABILITY: {durabilityStat}</li>
+        </ul>
+      </Card.Content>
+    </Card>
+>>>>>>> parent of 04cb3f7 (Styling Hero Icons)
   );
 };
 
